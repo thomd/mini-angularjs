e213fd7ae830b1c0fc1ae91d265e71bbe2f0a992
Thomas Duerr <thomduerr@gmail.com>
2013-11-15 16:52:32 +0100

    manage order dependency of watchers

--- mini-angular.js
+++ mini-angular.js
@@ -1,3 +1,5 @@
+// ------ library -------------------------------------------------------------------------------
+
 function Scope(){
 
   // a watcher is something that is notified when a change occurs in the scope
@@ -13,39 +15,71 @@ Scope.prototype.$watch = function(watchFn, listenerFn){
 }
 
 // run all teh watches which has been defined in the scope
-Scope.prototype.$digest = function(){
+Scope.prototype.$$digestOnce = function(){
   var self = this;
+  var dirty;
   _.forEach(this.$$watchers, function(watch){
     var newValue = watch.watchFn(self);
     var oldValue = watch.last;
     if(newValue !== oldValue){
       watch.listenerFn(newValue, oldValue, self);
+      dirty = true;
     }
     watch.last = newValue;
-  })
+  });
+  return dirty;
+}
+Scope.prototype.$digest = function(){
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while(dirty);
 }
 
-// -----------------------------------------------------------------------------------
+
+
+// ------ implementation ------------------------------------------------------------------------
 
 var scope = new Scope();
 scope.firstName = document.getElementById('firstname');
+scope.countInfo = document.getElementById('count-info');
 scope.counter = 0;
+
+// register counter watcher (dependent from first-name watcher)
+scope.$watch(
+  function(scope){
+    return scope.counter;
+  },
+  function(newValue, oldValue, scope){
+    if(scope.counter % 5 == 0){
+      scope.countInfo.innerHTML = scope.counter + " chars";
+    }
+  }
+);
+
+// register first-name watcher
 scope.$watch(
   function(scope){
     return S(scope.firstName.value).trim().s;
   },
   function(newValue, oldValue, scope){
     scope.counter ++;
+  }
+);
+
+// register logger
+scope.$watch(
+  function(){
     console.log(scope.counter);
   }
 );
-scope.$watch(
-  function(){console.log("digest fired");}
-);
-document.getElementById('firstname').addEventListener('keyup', function(ev){
-  scope.$digest();
-});
 
+
+// trigger digesting:w
+scope.firstName.addEventListener('keyup', function(ev){
+  scope.$digest();
+})
+scope.firstName.focus();
 
 
 
